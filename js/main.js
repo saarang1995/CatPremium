a73cfdd0379ba22d31f849e63d97d9cd26960cc5
Saarang Tiwari <SaarangT@Cybertech.Com>
2018-02-06 02:52:24 +0530

    added model code

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function()
+{
+	console.log("started");
+
+	// Model:
+
+	var model = (
+		{
+		name:"Cat 1",
+	   count:0,
+		 src:"res/cat1.jpg"	
+		},
+		{
+		name:"Cat 2",
+	   count:0,
+		 src:"res/cat2.jpg"	
+		},
+		{
+		name:"Cat 3",
+	   count:0,
+		 src:"res/cat3.jpg"	
+		},
+		{
+		name:"Cat 4",
+	   count:0,
+		 src:"res/cat4.jpg"	
+		},
+		{
+		name:"Cat 5",
+	   count:0,
+		 src:"res/cat5.jpg"	
+		}
+		);
+
+
+	// Octopus:
+
+	
+	
+
+});
