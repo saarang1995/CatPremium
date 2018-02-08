67c32360508a798d0209c3f2cc67b94cb533f8b8
Saarang Tiwari <SaarangT@Cybertech.Com>
2018-02-09 04:16:22 +0530

    cat premium MVC

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(function()
 {
 	console.log("started");
+	var countElement=new Array(5);
 
 	// Model:
 
-	var model = (
+	var model = [
 		{
 		name:"Cat 1",
 	   count:0,
@@ -18,7 +19,7 @@ $(function()
 		{
 		name:"Cat 3",
 	   count:0,
-		 src:"res/cat3.jpg"	
+		 src:"res/cat3.jpeg"	
 		},
 		{
 		name:"Cat 4",
@@ -30,12 +31,58 @@ $(function()
 	   count:0,
 		 src:"res/cat5.jpg"	
 		}
-		);
+		];
 
 
 	// Octopus:
+	var octopus={
 
+		getAllCatsList:function(){
+			return model;
+		},
+
+		getCatDetail:function(icopy){
+				return function(){
+						countElement[icopy]+=1;
+						clickelement[icopy].innerHTML="Number of Clicks = "  + countElement[icopy];
+							};							 }(i)
+		
+				
+
+		,
+
+		init:function(){
+			view.init();
+		}
+
+
+	};
+
+
+	// View:
+	var view={
+		init: function(){
+			
+			view.renderList();
+		},
+
+		renderList: function(){
+
+			var catlist=$('#cat-list');	
+				for (var i =0; i< octopus.getAllCatsList().length ; i++) {
+					countElement[i]=0;
+					var li= document.createElement('li');
+					var img=document.createElement('img');
+					img.setAttribute("id", "catlist-img");
+					img.src=octopus.getAllCatsList()[i].src;
+					li.appendChild(img);
+					catlist.append(li);
+
+					img.addEventListener('click',(octopus.getCatDetail(icopy)));
+				};
 	
-	
+}};
+		octopus.init();
+
 
 });
